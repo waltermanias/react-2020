716705c9c8c227db9f1508c380840568d0faf123
waltermanias <wmanias@gmail.com>
2020-07-28 09:08:00 -0300

    Remove dummy components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import UserInput from './components/UserInput/UserInput';
-import UserOutput from './components/UserOutput/UserOutput';
+
 
 const app = () => {
 
-    const [username, setUsername]= useState("walter.manias");
+  return (
+    <div className="App">
 
-    const changeNameHandler = (event) => {
-      setUsername(event.target.value)
-    }
-
-    return (
-      <div className="App">
-        <UserInput username={username} changed={changeNameHandler.bind(this)}></UserInput>
-        <UserOutput username={username}></UserOutput>
-        <UserOutput username={username}></UserOutput>
-        <UserOutput username={username}></UserOutput>
-      </div>
-    );
+    </div>
+  );
 }
 
 export default app;
